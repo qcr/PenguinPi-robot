3be3c2609007f275634b32f1b5abdad43f456af9
petercorke <peter.i.corke@gmail.com>
2018-09-08 05:50:40 +1000

    remove hardwired Atmel symbols read them from the pickle file change code to use string values for Atmel symbols remove unneeded opcode add logging minor changes for some opcodes

--- python/penguinPi.py
+++ python/penguinPi.py
@@ -5,11 +5,14 @@ import serial
 import struct
 import time
 import os
+import os.path
 import threading
 import queue
 import datetime
 import string
 import re
+import pickle
+import logging
 
 import traceback
 
@@ -26,134 +29,17 @@ DGRAM_MAX_LENGTH = 10 #bytes
 
 CRC_8_POLY = 0xAE
 
-#addresses
-AD_MOTORS 	= 0x01
-AD_MOTOR_A 		= 0x04
-AD_MOTOR_B 		= 0x05
-
-AD_SERVOS 	= 0x02
-AD_SERVO_A 		= 0x08
-AD_SERVO_B 		= 0x09
-
-AD_LEDS 	= 0x03
-AD_LED_R 		= 0x0C
-AD_LED_G 		= 0x0D
-AD_LED_B 		= 0x0E
-
-AD_DISPLAYS	= 0x04
-AD_DISPLAY_A 	= 0x10
-
-AD_BTNS 	= 0x05
-AD_BTN_A 		= 0x14
-AD_BTNS_B 		= 0x15
-AD_BTNS_C 		= 0x16
-
-AD_ADCS 	= 0x06
-AD_ADC_V 		= 0x18
-AD_ADC_C 		= 0x19
-
-AD_OLED		= 0x07
+debug_comms = False
+debug_comms = True
 
 
-
-
-AD_SYSTEM	= 0xF0				#System Control of the AVR i.e taking control of the I2C
-
-AD_ALL 		= 0xFF
-
-#opcodes
-#MOTOR
-MOTOR_SET_SPEED_DPS = 0x01
-MOTOR_SET_DEGREES = 0x02
-MOTOR_SET_DIRECTION = 0x03
-MOTOR_SET_GAIN_P = 0x04
-MOTOR_SET_GAIN_I = 0x05
-MOTOR_SET_GAIN_D = 0x06
-MOTOR_SET_ENC_MODE = 0x07
-MOTOR_SET_ENC = 0x08
-MOTOR_SET_CONTROL_MODE = 0x09
-
-MOTOR_GET_SPEED_DPS = 0x81
-MOTOR_GET_DEGREES = 0x82
-MOTOR_GET_DIRECTION = 0x83
-MOTOR_GET_GAIN_P = 0x84
-MOTOR_GET_GAIN_I = 0x85
-MOTOR_GET_GAIN_D = 0x86
-MOTOR_GET_ENC_MODE = 0x87
-MOTOR_GET_ENC = 0x88
-MOTOR_GET_CONTROL_MODE = 0x89
-
-#SERVO
-SERVO_SET_POSITION = 0x01
-SERVO_SET_STATE = 0x02
-SERVO_SET_MIN_RANGE = 0x03
-SERVO_SET_MAX_RANGE = 0x04
-SERVO_SET_MIN_PWM = 0x05
-SERVO_SET_MAX_PWM = 0x06
-
-SERVO_GET_POSITION = 0x81
-SERVO_GET_STATE = 0x82
-SERVO_GET_MIN_RANGE = 0x83
-SERVO_GET_MAX_RANGE = 0x84
-SERVO_GET_MIN_PWM = 0x85
-SERVO_GET_MAX_PWM = 0x86
-
-#LED
-LED_SET_STATE = 0x01
-LED_SET_BRIGHTNESS = 0x02
-LED_SET_COUNT = 0x03
-
-LED_GET_STATE = 0x81
-LED_GET_BRIGHTNESS = 0x82
-LED_GET_COUNT = 0x83
-
-#DISPLAY
-DISPLAY_SET_VALUE = 0x01
-DISPLAY_SET_DIGIT_1 = 0x02
-DISPLAY_SET_DIGIT_0 = 0x03
-DISPLAY_SET_MODE = 0x04
-
-DISPLAY_GET_VALUE = 0x81
-DISPLAY_GET_DIGIT_1 = 0x82
-DISPLAY_GET_DIGIT_0 = 0x83
-DISPLAY_GET_MODE = 0x84
-
-#OLED
-OLED_SET_IP_ETH_1  =0x01
-OLED_SET_IP_ETH_2  =0x02
-OLED_SET_IP_ETH_3  =0x03
-OLED_SET_IP_ETH_4  =0x04
-OLED_SET_IP_WLAN_1 =0x05
-OLED_SET_IP_WLAN_2 =0x06
-OLED_SET_IP_WLAN_3 =0x07
-OLED_SET_IP_WLAN_4 =0x08
-
-
-#BUTTON
-BUTTON_SET_PROGRAM_MODE = 0x01
-BUTTON_SET_PIN_MODE = 0x02
-
-BUTTON_GET_PROGRAM_MODE = 0x81
-BUTTON_GET_PIN_MODE = 0x82
-
-#ADC
-ADC_SET_SCALE = 0x01
-ADC_SET_RAW = 0x02
-ADC_SET_READING = 0x03
-
-ADC_GET_SCALE = 0x81
-ADC_GET_RAW = 0x82
-ADC_GET_READING = 0x83
-
-#GLOBAL
-ALL_STOP = 0xFF
-CLEAR_DATA = 0xEE
-SET_RESET = 0xDD
-GET_RESET = 0x11
-ALL_SET_DIP = 0x20
-ALL_GET_DIP = 0xA0
-ALL_SET_MOTORS = 0x23
-ALL_GET_MOTORS = 0xA3
+symbols = os.path.join(os.path.dirname(__file__), 'atmel-symbols.pickle')
+try:
+    with open( symbols, 'rb') as file:
+        atmel_symbols = pickle.Unpickler(file).load();
+except:
+    logging.critical("can't load Atmel symbol table: " + symbols, file=sys.stderr)
+    sys.exit(1)
 
 class UART(object):
     """Setup UART comunication between the raspberry pi and the microcontroler
@@ -161,7 +47,7 @@ class UART(object):
     def __init__(self, port='/dev/serial0', baud=115200):
 
         try:
-            self.ser = serial.Serial(    port = port,
+            self.ser = serial.Serial(   port = port,
                                         baudrate = baud,
                                         parity = serial.PARITY_NONE,
                                         stopbits = serial.STOPBITS_ONE,
@@ -171,7 +57,7 @@ class UART(object):
             )
         except serial.serialutil.SerialException:
             # somebody else has the port open, give up now
-            print("can't acquire exclusive access to communications port", file=sys.stderr)
+            logging.critical("can't acquire exclusive access to communications port", file=sys.stderr)
             sys.exit(1)
 
         self.queue = queue.Queue()
@@ -191,8 +77,7 @@ class UART(object):
         self.ser.reset_output_buffer()
 
         #display that connection is started
-        print("Transmitting on " + self.ser.name)
-        print("Serial settings: " + str(self.ser.baudrate) + " " + str(self.ser.bytesize) + " " + self.ser.parity + " " + str(self.ser.stopbits))
+        logging.info("Communicating on " + self.ser.name + " at " + str(self.ser.baudrate) + " " + str(self.ser.bytesize) + " " + self.ser.parity + " " + str(self.ser.stopbits))
 
         #start the recive thread
         self.receive_thread.start()
@@ -240,7 +125,7 @@ class UART(object):
 
                     dgram = self.ser.read(paylenint-1)  # read the rest
                     if len(dgram) != paylenint-1:
-                        print('short read', len(dgram), paylenint-1)
+                        logging.error('short read', len(dgram), paylenint-1)
 
                     dgram = paylen + dgram   # datagram is length + the rest
 
@@ -255,8 +140,7 @@ class UART(object):
                         self.queue.put(dgram)
                     else:
                         # bad CRC, print some diagnostics
-                        print("ERROR: CRC Failed ", hex(crcCalc), " received ", hex(crcDgram))
-                        print_hex(dgram, ' DG')
+                        logging.error("CRC Failed ", hex(crcCalc), " received ", hex(crcDgram), datagram2str(dgram));
                         self.queue.put(None)
                 else: 
                     # text from the Atmel, perhaps an error message
@@ -272,7 +156,7 @@ class UART(object):
                         print(chr(ord(byte)), end="")
                             
             except serial.SerialException:
-                print("--- caught serial port error")
+                logging.error("--- caught serial port error")
 
 #create UART object
 uart = UART("/dev/serial0", 115200)
@@ -290,7 +174,7 @@ def close():
     stop_all()
     time.sleep(1)
     uart.stop()
-    print("UART stopped and closed")
+    logging.info("UART stopped and closed")
 
 
 def crc8(word, length):
@@ -307,10 +191,8 @@ def crc8(word, length):
     return crc
 
 
-def print_hex(bin, label=''):
-    '''print hex value
-    '''
-    print(label + ': ', " ".join(hex(n) for n in bin))
+def datagram2str(bin):
+    return ".".join(hex(n) for n in bin)
 
 re_type = re.compile(r"""(?P<t>[a-z0-9]+)  # type
                  (\[
@@ -354,25 +236,39 @@ def send_datagram(address, opCode, payload=None, paytype='', rxtype=''):
     '''Create and send the datagram to sent to the microcontroler
     '''
 
+    # convert strings to numeric values for the Atmel
+    if not address in atmel_symbols:
+        print("address symbol %s not defined" % address, file=sys.stderr);
+        sys.exit(1)
+    address = atmel_symbols[address]
+
+    if not opCode in atmel_symbols:
+        print("opcode symbol %s not defined" % opCode, file=sys.stderr);
+        sys.exit(1)
+    opCode = atmel_symbols[opCode]
+
     # turn the payload type into a format string for struct.pack
     format = get_struct_format(paytype)
 
     # if the args are a list, append that to the address and opcode
     args = [address, opCode]
-    if payload:
+    if payload is not None:
         if type(payload) is list:
             args.extend(payload)
         else:
             args.append(payload)
-
     # convert to binary string
     dgram = struct.pack('!BB'+format, *args)
 
     # build up the rest of the datagram
-    length = (len(dgram) + 2)
+    length = len(dgram) + 2
     dgram = (struct.pack("!B", length)) + dgram
     crc = crc8(dgram, length-1)
     dgram = dgram + (struct.pack("!B", crc))
+
+    if debug_comms:
+        logging.debug(datagram2str(dgram))
+
 
     with comms_mutex:
         # the following code can be executed by only one thread at a time
@@ -395,9 +291,11 @@ def send_datagram(address, opCode, payload=None, paytype='', rxtype=''):
                 return None
 
             if dgram:
-                result = extract_payload(dgram, address, opCode&0x7f, rxtype)
+                result = extract_payload(dgram, address, opCode, rxtype)
                 if result is None:
                     print('-- error in extract_payload')
+                if len(result) == 1:
+                    result = result[0]
             else:
                 # datagram is None, indicates failure at the packet RX end
                 print('-- null entry in queue')
@@ -417,11 +315,10 @@ def extract_payload(bin, address, opcode, paytype):
 
     if bin[1] == address and bin[2] == opcode:
         # check that address and opcode match
-        ret = struct.unpack(format, bin[3:])
+        ret = struct.unpack('!'+format, bin[3:])
         return ret
     else:
-        print("address/opcode mismatch:", bin, " expected ", [address,opcode])
-        print_hex(bin, 'packet')
+        print("address/opcode mismatch:", bin, " expected ", [address,opcode], datagram2str(bin))
         return None
 
 def get_dip():
@@ -429,20 +326,20 @@ def get_dip():
        Switch 1 is the high-order bit.`
        ON means 1.
     '''
-    dip = send_datagram(AD_ALL, ALL_GET_DIP, rxtype='uint8') & 0xff
+    dip = send_datagram('AD_ALL', 'ALL_GET_DIP', rxtype='uint8') & 0xff
     return dip
 
 def motor_setget(speedL, speedR):
     # send the motor speeds to Atmel
-    encoders = send_datagram(AD_ALL, ALL_GET_MOTORS, [speedR,speedL], 'int16[2]', rxtype='uint16[2]')
+    encoders = send_datagram('AD_ALL', 'ALL_GET_ENC_SET_SPEED', [speedL,speedR], 'int8[2]', rxtype='uint16[2]')
 
     return encoders
 
 def stop_all():
-    send_datagram(AD_ALL, ALL_STOP)
+    send_datagram('AD_ALL', 'ALL_STOP')
 
 def clear_data():
-    send_datagram(AD_ALL, CLEAR_DATA)
+    send_datagram('AD_ALL', 'ALL_CLEAR_DATA')
 
 ### --- Device Classes --- ###
 '''
@@ -451,7 +348,7 @@ Motor Object
     to degrees in your own code
 '''
 class Motor(object):
-    '''Motor class uesed with the penguin pi
+    '''Motor class used with the penguin pi
     '''
 
     #creates and instance of Motor
@@ -467,72 +364,59 @@ class Motor(object):
         self.gainD = 0
 
 #SETTERS
-    def set_power(self, speed):
+    def set_speed(self, speed):
         self.speedDPS = speed
-        send_datagram(self.address, MOTOR_SET_SPEED_DPS, speed, 'int16')
+        send_datagram(self.address, 'MOTOR_SET_SPEED', speed, 'int16')
 
     #not implmented on the micro controler
     def set_degrees(self, degrees):
         self.degrees = degrees
-        send_datagram(self.address, MOTOR_SET_DEGREES, degrees, 'int16')
-
-    def set_direction(self, direction):
-        self.dir = direction
-        send_datagram(self.address, MOTOR_SET_DIRECTION, direction, 'uint8')
+        send_datagram(self.address, 'MOTOR_SET_DEGREES', degrees, 'int16')
 
     def set_encoder_mode(self, mode):
         self.encoderMode = mode
-        send_datagram(self.address, MOTOR_SET_ENC_MODE, mode, 'uint8')
+        send_datagram(self.address, 'MOTOR_SET_ENC_MODE', mode, 'uint8')
 
     #works with set_degrees not implmented on the microcontroler
     def set_PID(self, kP=0, kI=0, kD=0):
         self.gainP = kP
         self.gainI = kI
         self.gainD = kD
-        send_datagram(self.address, MOTOR_SET_GAIN_P, kP, 'float')
-        send_datagram(self.address, MOTOR_SET_GAIN_I, kI, 'float')
-        send_datagram(self.address, MOTOR_SET_GAIN_D, kD, 'float')
+        send_datagram(self.address, 'MOTOR_SET_GAIN_P', kP, 'float')
+        send_datagram(self.address, 'MOTOR_SET_GAIN_I', kI, 'float')
+        send_datagram(self.address, 'MOTOR_SET_GAIN_D', kD, 'float')
 
 #GETTERS
     def get_speed(self):
-        return send_datagram(self.address, MOTOR_GET_SPEED_DPS, rxtype='int16')
-
-    def get_ticks(self):
-        self.degrees = send_datagram(self.address, MOTOR_GET_DEGREES, rxtype='int16')
-        return self.degrees
+        self.speed = send_datagram(self.address, 'MOTOR_GET_SPEED', rxtype='int16')
+        return self.speed
 
     def get_encoder(self):
-        return send_datagram(self.address, MOTOR_GET_ENC, rxtype='int16')
-
-    def get_direction(self):
-        self.dir = send_datagram(self.address, MOTOR_GET_DIRECTION, rxtype='uint8')
-        return self.dir
+        self.encoder = send_datagram(self.address, 'MOTOR_GET_ENC', rxtype='int16')
+        return self.encoder
 
     def get_encoder_mode(self):
-        self.encoderMode = send_datagram(self.address, MOTOR_GET_ENC_MODE, rxtype='uint8')
+        self.encoderMode = send_datagram(self.address, 'MOTOR_GET_ENC_MODE', rxtype='uint8')
         return self.encoderMode
 
     def get_PID(self):
         kP=kI=kD = -1
 
-        kP = send_datagram(self.address, MOTOR_GET_GAIN_P, rxtype='float')
+        kP = send_datagram(self.address, 'MOTOR_GET_GAIN_P', rxtype='float')
         self.gainP = kP
 
-        kI = send_datagram(self.address, MOTOR_GET_GAIN_I, rxtype='float')
+        kI = send_datagram(self.address, 'MOTOR_GET_GAIN_I', rxtype='float')
         self.gainI = kI
 
-        kD = send_datagram(self.address, MOTOR_GET_GAIN_D, rxtype='float')
+        kD = send_datagram(self.address, 'MOTOR_GET_GAIN_D', rxtype='float')
         self.gainD = kD
 
         return kP, kI, kD
 
     def get_all(self):
         self.get_speed()
-        self.get_ticks()
-        self.get_direction()
-        self.get_PID()
+        self.get_encoder()
         self.get_encoder_mode()
-
 
 '''
 Servo Object
@@ -555,36 +439,36 @@ class Servo(object):
 #SETTERS
     def set_position(self, position):
         self.position = position
-        send_datagram(self.address, SERVO_SET_POSITION, position, 'int16')
+        send_datagram(self.address, 'SERVO_SET_POSITION', position, 'int16')
 
     def set_state(self, state):
         self.state = state
-        send_datagram(self.address, SERVO_SET_STATE, state, 'uint8')
+        send_datagram(self.address, 'SERVO_SET_STATE', state, 'uint8')
 
     def set_range(self, minimum, maximum):
         self.minRange = minimum
         self.maxRange = maximum
-        send_datagram(self.address, SERVO_SET_MIN_RANGE, minimum, 'int16')
-        send_datagram(self.address, SERVO_SET_MAX_RANGE, maximum, 'int16')
+        send_datagram(self.address, 'SERVO_SET_MIN_RANGE', minimum, 'int16')
+        send_datagram(self.address, 'SERVO_SET_MAX_RANGE', maximum, 'int16')
 
 #GETTERS
     def get_position(self):
-        self.position = send_datagram(self.address, SERVO_GET_POSITION, rxtype='int16')
+        self.position = send_datagram(self.address, 'SERVO_GET_POSITION', rxtype='int16')
         return self.position
 
     def get_state(self):
-        self.state = send_datagram(self.address, SERVO_GET_STATE, rxtype='uint8')
+        self.state = send_datagram(self.address, 'SERVO_GET_STATE', rxtype='uint8')
         return self.state
 
     def get_range(self):
-        self.minRange = send_datagram(self.address, SERVO_GET_MIN_RANGE, rxtype='int16')
-        self.maxRange = send_datagram(self.address, SERVO_GET_MAX_RANGE, rxtype='int16')
+        self.minRange = send_datagram(self.address, 'SERVO_GET_MIN_RANGE', rxtype='int16')
+        self.maxRange = send_datagram(self.address, 'SERVO_GET_MAX_RANGE', rxtype='int16')
 
         return self.minRange, self.maxRange
 
     def get_PWM_range(self):
-        self.minPWMRange = send_datagram(self.address, SERVO_GET_MIN_PWM, rxtype='int16')
-        self.maxPWMRange = send_datagram(self.address, SERVO_GET_MAX_PWM, rxtype='int16')
+        self.minPWMRange = send_datagram(self.address, 'SERVO_GET_MIN_PWM', rxtype='int16')
+        self.maxPWMRange = send_datagram(self.address, 'SERVO_GET_MAX_PWM', rxtype='int16')
 
         return self.minPWMRange, self.maxPWMRange
 
@@ -609,34 +493,16 @@ class LED(object):
 #SETTERS
     def set_state(self, state):
         self.state = state
-        send_datagram(self.address, LED_SET_STATE, state, 'uint8')
-
-    def set_brightness(self, brightness):
-        self.brightness = brightness
-        send_datagram(self.address, LED_SET_BRIGHTNESS, brightness, 'uint8')
+        send_datagram(self.address, 'LED_SET_STATE', state, 'uint8')
 
     def set_count(self, count):
         self.count = count
-        send_datagram(self.address, LED_SET_COUNT, count, 'uint16')
+        send_datagram(self.address, 'LED_SET_COUNT', count, 'uint8')
 
 #GETTERS
     def get_state(self):
-        self.state = send_datagram(self.address, LED_GET_STATE, rxtype='uint8')
+        self.state = send_datagram(self.address, 'LED_GET_STATE', rxtype='uint8')
         return self.state
-
-    def get_brightness(self):
-        self.brightness = send_datagram(self.address, LED_GET_BRIGHTNESS, rxtype='uint8')
-        return self.brightness
-
-    def get_count(self):
-        self.count = send_datagram(self.address, LED_GET_COUNT, rxtype='uint16')
-        return self.count
-
-    def get_all(self):
-        self.get_state()
-        self.get_brightness()
-        self.get_count()
-
 
 '''
 Display object
@@ -752,25 +618,25 @@ class AnalogIn(object):
 #SETTERS
     def set_scale(self, scale):
         self.scale = scale
-        send_datagram(self.address, ADC_SET_SCALE, scale, 'float')
+        send_datagram(self.address, 'ADC_SET_SCALE', scale, 'float')
 
 #GETTERS
     def get_scale(self):
-        self.scale = send_datagram(self.address, ADC_GET_SCALE, rxtype='float')
+        self.scale = send_datagram(self.address, 'ADC_GET_SCALE', rxtype='float')
         return self.scale
 
-    def get_raw(self):
-        self.raw = send_datagram(self.address, ADC_GET_RAW, rxtype='uint16')
-        return self.raw
-
     def get_value(self):
-        self.value = send_datagram(self.address, ADC_GET_READING, rxtype='float')
+        self.value = send_datagram(self.address, 'ADC_GET_VALUE', rxtype='uint16')
         return self.value
+
+    def get_smooth(self):
+        self.smooth = send_datagram(self.address, 'ADC_GET_SMOOTH', rxtype='float')
+        return self.smooth
 
     def get_all(self):
         self.get_scale()
-        self.get_raw()
         self.get_value()
+        self.get_smooth()
 		
 		
 '''
@@ -781,36 +647,17 @@ class OLED(object):
 
     def __init__(self, address):
         self.address = address
-        self.ip_eth_1   = 0
-        self.ip_eth_2   = 0
-        self.ip_eth_3   = 0
-        self.ip_eth_4   = 0
-        self.ip_wlan_1  = 0
-        self.ip_wlan_2  = 0
-        self.ip_wlan_3  = 0
-        self.ip_wlan_4  = 0
+        self.ip_eth   = 0
+        self.ip_wlan  = 0
 		
 #SETTERS
-    def set_ip_eth( self, ip_addr ):
+    def set_ip_eth( self, ipaddr ):
         octets = [int(x) for x in ipaddr.split('.')]
-        self.ip_eth_1 = octets[0]
-        self.ip_eth_2 = octets[1]
-        self.ip_eth_3 = octets[2]
-        self.ip_eth_4 = octets[3]
-
-        send_datagram(self.address, OLED_SET_IP_ETH_1, self.ip_eth_1, 'uint8')
-        send_datagram(self.address, OLED_SET_IP_ETH_2, self.ip_eth_2, 'uint8')
-        send_datagram(self.address, OLED_SET_IP_ETH_3, self.ip_eth_3, 'uint8')
-        send_datagram(self.address, OLED_SET_IP_ETH_4, self.ip_eth_4, 'uint8')
+        print(octets)
+        send_datagram(self.address, 'OLED_SET_IP_ETH', octets, 'uint8[4]')
 
     def set_ip_wlan( self, ipaddr ):
         octets = [int(x) for x in ipaddr.split('.')]
-        self.ip_wlan_1 = octets[0]
-        self.ip_wlan_2 = octets[1]
-        self.ip_wlan_3 = octets[2]
-        self.ip_wlan_4 = octets[3]
+        send_datagram(self.address, 'OLED_SET_IP_WLAN', octets, 'uint8[4]')
 
-        send_datagram(self.address, OLED_SET_IP_WLAN_1, self.ip_wlan_1, 'uint8')
-        send_datagram(self.address, OLED_SET_IP_WLAN_2, self.ip_wlan_2, 'uint8')
-        send_datagram(self.address, OLED_SET_IP_WLAN_3, self.ip_wlan_3, 'uint8')
-        send_datagram(self.address, OLED_SET_IP_WLAN_4, self.ip_wlan_4, 'uint8')
+logging.basicConfig(format='%(asctime)s %(levelname)s  %(message)s', level=logging.INFO)
