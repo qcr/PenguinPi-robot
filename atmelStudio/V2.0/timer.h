da6e21248f489670c782e465a9185b4a24d5d5cd
petercorke <peter.i.corke@gmail.com>
2018-09-07 19:25:37 +1000

    Update time structure
    now contains: seconds, milliseconds, 20MHz counter

--- atmelStudio/V2.0/timer.h
+++ atmelStudio/V2.0/timer.h
@@ -7,26 +7,29 @@
 #ifndef __timer_h__
 #define __timer_h__
 
-typedef uint32_t timer_t;
+typedef struct {
+    uint32_t sec;   // seconds
+    uint16_t ms;    // milliseconds
+    uint16_t clock; // 20MHz counter
+} timer_t;
+
 
 // global
 
-volatile extern timer_t	system_timer;
+void timer_get(timer_t *t);
+uint32_t timer_diff_us(timer_t *t1, timer_t *t0);
 
-timer_t timer_get();
-timer_t timer_diff(timer_t t1, timer_t t0);
-uint32_t timer_ms(timer_t t);
-
-typedef struct _stats {
+typedef struct {
     uint32_t    sum;
     uint32_t    sum2;
     uint32_t    n;
     uint32_t    max;
-    } stats_t;
+} stats_t;
 
 void stats_init(stats_t *stats);
-void stats_add(stats_t *stats, timer_t val);
-timer_t stats_mean(stats_t *stats);
-timer_t stats_var(stats_t *stats);
+void stats_add(stats_t *stats, uint32_t val);
+uint32_t stats_mean(stats_t *stats);
+uint32_t stats_var(stats_t *stats);
+uint32_t stats_std(stats_t *stats);
 
 #endif
