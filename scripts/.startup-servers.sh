e83798aa70820f7187a1059b05b086cd1b6ff121
Steve <steven.bulmer@qut.edu.au>
2019-01-15 20:21:57 +1000

    Reflect new path to ppweb server

--- scripts/.startup-servers.sh
+++ scripts/.startup-servers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #python3 /home/pi/PenguinPi/EGB439/python/server-camera.py &
 #python3 /home/pi/PenguinPi/EGB439/python/server-motors.py &
-(cd /home/pi/EGB439/python/flask; python3 ppweb.py &)
+(cd /home/pi/EGB439/python/robot; python3 ppweb.py &)
 python3 /home/pi/EGB439/scripts/GPIOSoftShutdown.py &
