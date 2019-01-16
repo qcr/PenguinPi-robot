9447ee965e2f002c4936ade268626f1d236d87dc
Steve <steven.bulmer@qut.edu.au>
2019-01-16 17:11:24 +1000

    localiser scripts git mv

--- scripts/.localiser.sh
+++ scripts/.localiser.sh
@@ -4,6 +4,6 @@ python3 /home/pi/egb439/python/robot/old-servers/server-camera.py &
 #(cd /home/pi/egb439/python/localiser; python3 ppweb.py)
 # # python3 /home/pi/PenguinPi/EGB439/python/server-motors.py
 while true; do
-     python3 /home/pi/egb439/scripts/image_loc.py
+     python3 /home/pi/egb439/python/localiser/image_loc.py
 done
 #
