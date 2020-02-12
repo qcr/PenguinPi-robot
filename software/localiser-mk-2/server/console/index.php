1d80cb7516a28e204ca42ac6a2e76807315c048f
Jenna Riseley <jenna@lyro.io>
2020-02-12 20:24:34 +1000

    Started setting up web console

---
+++ software/localiser-mk-2/server/console/index.php
@@ -0,0 +1,25 @@
+<?php  ?>
+<!doctype html>
+
+<html>
+    <head>
+        <script type="text/javascript" src="jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="drag.js"></script>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+    <body>
+    
+    <div id="mydiv">
+        <div id="mydivheader">
+            Click here to move
+        </div>
+        <p>Move</p>
+        <p>this</p>
+        <p>DIV</p>
+    </div>
+
+    </body>
+</html>
+
+
--- software/localiser-mk-2/setup.sh
+++ software/localiser-mk-2/setup.sh
@@ -6,16 +6,19 @@ WEBAPP_LOCATION=/var/www/EGB439
 HOST=127.0.0.1
 
 echo "Setting up server directory structure in $WEBAPP_LOCATION..."
-sudo rm -r $WEBAPP_LOCATION
+sudo rm -r -f $WEBAPP_LOCATION
 sudo mkdir -p $WEBAPP_LOCATION
 sudo mkdir -p $WEBAPP_LOCATION/camera/get
+sudo mkdir -p $WEBAPP_LOCATION/console
 
 echo "Copying nginx configuration..."
 sudo cp server/default /etc/nginx/sites-available/default
 
+echo "Copying web console..."
+sudo cp server/console/* $WEBAPP_LOCATION/console
+
 # Copy php settings
-# sudo cp server/php.ini /etc/php/7.2/fpm/php.ini
-# sudo cp server/index.php $WEBAPP_LOCATION
+sudo cp server/php.ini /etc/php/7.2/fpm/php.ini
 
 #echo "Creating build directory ..."
 #mkdir -p build
@@ -24,9 +27,6 @@ sudo cp server/default /etc/nginx/sites-available/default
 #cmake ..
 #make clean
 #make
-
-
-
 
 
 echo "DONE"
