93dfafcae36e0de92c8712d3c37eff69cd04c59f
Jenna <j2.riseley@qut.edu.au>
2020-02-14 13:29:36 +1000

    Install pkgs before setting user permissions

--- software/localiser-mk-2/configure.sh
+++ software/localiser-mk-2/configure.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+echo "Installing packages..."
+pkg_list="nginx net-tools libfcgi libfcgi-dev build-essential libc-dev libboost-all-dev php7.2 php-fpm php-mysql cmake git \
+ libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev \
+ libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev libcanberra-gtk-module libcanberra-gtk3-module"
+
+for package in $pkg_list
+do
+    if sudo apt -y install $package >/dev/null 2> /dev/null; then
+        echo $package
+    else
+        echo "$package **** FAILED ****"
+    fi
+done
+
 username=$USER
 echo "Checking permissions..."
 
@@ -17,19 +31,5 @@ do
     sudo chgrp -R www-data $f
     sudo chown -R www-data: $f
     sudo chmod -R 2775 $f
-done
-
-echo "Installing packages..."
-pkg_list="nginx net-tools libfcgi libfcgi-dev build-essential libc-dev libboost-all-dev php7.2 php-fpm php-mysql cmake git \
- libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev \
- libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev libcanberra-gtk-module libcanberra-gtk3-module"
-
-for package in $pkg_list
-do
-    if sudo apt -y install $package >/dev/null 2> /dev/null; then
-        echo $package
-    else
-        echo "$package **** FAILED ****"
-    fi
 done
 
