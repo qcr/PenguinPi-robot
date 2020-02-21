207c1f9acf7354782eef09ef1ec5fdbe175b3a8e
Jenna Riseley <jenna@lyro.io>
2020-02-22 09:48:55 +1000

    Trialing streaming from pi and image processing on computer

--- software/localiser/install.sh
+++ software/localiser/install.sh
@@ -14,6 +14,17 @@ debug=FALSE
 BUILD=TRUE
 SET_PERMISSIONS=TRUE
 
+arch=$(dpkg --print-architecture)
+echo "Detected architecture $arch"
+
+if [ $arch = "armhf" ]; then 
+    echo "Building for raspberry pi with camera"
+    camera=TRUE
+else 
+    echo "Building for image processing + web server"
+    camera=FALSE
+fi
+
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
@@ -41,6 +52,10 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 
+echo "Making folder in /opt/ for streaming script..."
+mkdir -p $SCRIPT_INSTALL_DIR
+
+
 if [ $SET_PERMISSIONS = "TRUE" ]; then 
 
     echo "Checking permissions..."
@@ -60,51 +75,8 @@ if [ $SET_PERMISSIONS = "TRUE" ]; then
 
 fi 
 
-echo "Setting up server directory structure in $WEB_DIR..."
-sudo rm -r -f $WEB_DIR
-sudo mkdir -p $WEB_DIR
-
-echo "Copying php settings..."
-PHP_VERSION=$(php -r "echo PHP_VERSION;" | grep --only-matching --perl-regexp "7.\d+")
-cp config/php.ini /etc/php/$PHP_VERSION/fpm/php.ini
-
-echo "Copying nginx configuration..."
-REPLACE_PHP="s/<PHP_VERSION>/${PHP_VERSION}/g"
-cp config/default /etc/nginx/sites-available/default
-sed -i $REPLACE_PHP /etc/nginx/sites-available/default
-
-echo "Copying application..."
-cp -r www/* $WEB_DIR
-
-echo "Making folder in /opt/ for streaming script..."
-mkdir -p $SCRIPT_INSTALL_DIR
-sudo cp scripts/* $SCRIPT_INSTALL_DIR
-chmod +x $SCRIPT_INSTALL_DIR/*
-
-for f in /var/www /etc/nginx /etc/php $WEB_DIR $SCRIPT_INSTALL_DIR
-do
-    echo "Adding $f r/w permissions for $WEB_USER..."
-    sudo chgrp -R $WEB_USER $f
-    sudo chown -R $WEB_USER: $f
-    sudo chmod -R 2775 $f
-done
-
-echo "Restarting nginx.."
-sudo service nginx reload
-
 
 if [ $BUILD = "TRUE" ]; then 
-
-    arch=$(dpkg --print-architecture)
-    echo "Detected architecture $arch"
-
-    if [ $arch = "armhf" ]; then 
-        echo "Compiling with camera module"
-        camera=TRUE
-    else 
-        echo "Compiling without camera module"
-        camera=FALSE
-    fi
 
     BUILD_DIR=$(mktemp -d -t)
     echo "Created temporary directory " $BUILD_DIR
@@ -125,22 +97,67 @@ if [ $BUILD = "TRUE" ]; then
 
 fi 
 
-echo "Setting up streaming service..."
-STREAM_SERVICE_FILE=videostream.service
-sudo cp config/$STREAM_SERVICE_FILE /etc/systemd/system/$STREAM_SERVICE_FILE
-sudo chmod 644 /etc/systemd/system/$STREAM_SERVICE_FILE
+if [ $camera = "TRUE" ]; then
 
-echo "Setting up image processing service..."
-SERVICE_FILE=localiser.service
-sudo cp config/$SERVICE_FILE /etc/systemd/system/$SERVICE_FILE
-sudo chmod 644 /etc/systemd/system/$SERVICE_FILE
+    sudo cp scripts/videostream* $SCRIPT_INSTALL_DIR
+
+else 
+# Build for desktop computer
+    echo "Setting up server directory structure in $WEB_DIR..."
+    sudo rm -r -f $WEB_DIR
+    sudo mkdir -p $WEB_DIR
+
+    echo "Copying php settings..."
+    PHP_VERSION=$(php -r "echo PHP_VERSION;" | grep --only-matching --perl-regexp "7.\d+")
+    cp config/php.ini /etc/php/$PHP_VERSION/fpm/php.ini
+
+    echo "Copying nginx configuration..."
+    REPLACE_PHP="s/<PHP_VERSION>/${PHP_VERSION}/g"
+    cp config/default /etc/nginx/sites-available/default
+    sed -i $REPLACE_PHP /etc/nginx/sites-available/default
+
+    echo "Copying application..."
+    cp -r www/* $WEB_DIR
+
+    sudo cp scripts/localiser* $SCRIPT_INSTALL_DIR
+
+    for f in /var/www /etc/nginx /etc/php $WEB_DIR $SCRIPT_INSTALL_DIR
+    do
+        echo "Adding $f r/w permissions for $WEB_USER..."
+        sudo chgrp -R $WEB_USER $f
+        sudo chown -R $WEB_USER: $f
+        sudo chmod -R 2775 $f
+    done
+
+    echo "Restarting nginx.."
+    sudo service nginx reload
+fi 
+
+chmod +x $SCRIPT_INSTALL_DIR/*
+
+if [ $camera = "TRUE" ]; then
+    echo "Setting up streaming service..."
+    STREAM_SERVICE_FILE=videostream.service
+    sudo cp config/$STREAM_SERVICE_FILE /etc/systemd/system/$STREAM_SERVICE_FILE
+    sudo chmod 644 /etc/systemd/system/$STREAM_SERVICE_FILE
+    sudo systemctl start videostream
+    sudo systemctl enable videostream
+else 
+    echo "Setting up image processing service..."
+    SERVICE_FILE=localiser.service
+    sudo cp config/$SERVICE_FILE /etc/systemd/system/$SERVICE_FILE
+    sudo chmod 644 /etc/systemd/system/$SERVICE_FILE
+    sudo systemctl start localiser
+    sudo systemctl enable localiser
+fi 
 
 sudo systemctl daemon-reload
 
-echo "Starting systemd services.."
+echo "Done"
 
-sudo systemctl enable videostream
-sudo systemctl enable localiser
+
+
+
 
 
 
