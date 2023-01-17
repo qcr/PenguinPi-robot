b4467549ecaad8441f2d4a96b68409d96ffb030f
Tobias Fischer <tobias.fischer@qut.edu.au>
2023-01-17 15:08:40 +1000

    Working version of ppweb using picamera2 on 64-bit OS

--- software/python/robot/test/camtest.py
+++ software/python/robot/test/camtest.py
@@ -1,9 +1,12 @@
-import picamera
+#!/usr/bin/python3
 
-IM_WIDTH = 320
-IM_HEIGHT = 240
+from picamera2 import Picamera2
 
-camera = picamera.PiCamera()
-camera.resolution = (IM_WIDTH, IM_HEIGHT)
+picam2 = Picamera2()
 
-camera.capture('z.png', format='png')
+# Capture one image with the default configurations.
+picam2.start_and_capture_file("test.jpg")
+
+# Capture 3 images. Use a 0.5 second delay after the first image.
+picam2.start_and_capture_files("test{:d}.jpg", num_files=3, delay=0.5)  # noqa
+
